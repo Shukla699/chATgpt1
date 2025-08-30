e875badc5916b950547392af9cce6d3c6b4956bc
shashwatshukla <shashwatshukla159@gmail.com>
2025-08-30 19:20:58 +0530

    latest update

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,8 @@ app.use("/api/auth", authRoute)
 app.use("/api/chat", chatRoute)
 
 app.use(express.static(path.join(__dirname, "../public")))
-app.get("*", (req, res) => {
+
+app.get("*name", (req, res) => {
     res.sendFile(path.join(__dirname, "../public", "index.html"))
 })
 
